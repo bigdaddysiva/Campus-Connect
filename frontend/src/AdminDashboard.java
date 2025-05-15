6f886b284404a1d307be7a34e9e74e75b2fc2b39
Dhruv Siva <99091131+bigdaddysiva@users.noreply.github.com>
2025-05-14 20:20:34 -0400

    Create AdminDashboard.java

---
+++ frontend/src/AdminDashboard.java
@@ -0,0 +1,6 @@
+public class AdminDashboard {
+    public static void display(String username) {
+        System.out.println("Welcome, admin " + username + "!");
+        // Display dashboard options to post announcements or upload resources
+    }
+}
